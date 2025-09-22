32c1a34c67e45ed701118cb8fbd6e5040951a838
shreyashmahalle <128777174+shreyashmahalle@users.noreply.github.com>
2025-09-22 10:55:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //your JS code here. If required.
-let element = document.getElmentById("level");
-let level = 0;
-while(element){
-	level++;
-	elment = element.parentElement;
+let element = document.getElementById("level");  // target element
+let level = 0;  // counter
+
+while (element) {   // go up until <html>
+    level++;
+    element = element.parentElement;
 }
-document.write("The level of the element is:" + level);
+
+document.write("The level of the element is: " + level);
